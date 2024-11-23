49ae8cb627d224ce6b40d17b6a6b3c9c33510c31
Natanias <nataniasdopb@gmail.com>
2024-11-23 00:34:24 -0300

    Index atualizado

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,42 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import { Alert, FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Task } from "../src/components/Task";
 import { CardNumber } from "../src/components/CardNumber";
 import { InputAddTask } from "../src/components/InputAddTask";
+import { useState } from "react";
 
 export default function Screen() {
+
+  const [tasks, setTasks] = useState<{description: string; check: boolean}[]>([]);
+  const [taskText, setTaskText] = useState("");
+
+  function handleTaskAdd(){
+    if(taskText.length > 0){
+      return Alert.alert("Erro", "Tarefa está sem descricão");
+    }
+    if(tasks.some((task) => task.description === taskText)){
+      return Alert.alert("Erro", "Tarefa já existe");
+    }
+    const newTask = {description: taskText, check: false};
+    setTasks([...tasks, newTask]);
+    setTaskText('');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <InputAddTask />
       <CardNumber />
-      <Task />
+      <FlatList 
+        data={tasks} 
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) =>( <Task />)} 
+        ListEmptyComponent={() => (
+          <View>
+            <Text>Tarefa não cadastrada!</Text>
+            <Text>Crie uma tarefa.</Text>
+          </View> 
+        
+      )}/>
+
     </SafeAreaView>
   );
 }
@@ -20,6 +48,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
+    paddingTop: 64,
     gap: 16,
   },
 });
